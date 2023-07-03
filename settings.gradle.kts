c48335f53cefe6f7201ce732c2b5ae7ba43a94da
CursedFlames <18627001+CursedFlames@users.noreply.github.com>
2023-07-03 18:49:02 +1200

    monorepo initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,29 @@
+pluginManagement {
+	repositories {
+		gradlePluginPortal()
+		maven {
+			name = "Forge"
+			setUrl("https://maven.minecraftforge.net/")
+		}
+		maven {
+			name = "Fabric"
+			setUrl("https://maven.fabricmc.net/")
+		}
+		maven {
+			name = "Sponge Snapshots"
+			setUrl("https://repo.spongepowered.org/repository/maven-public/")
+		}
+	}
+}
+
+plugins {
+	id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+// This should match the folder name of the project, or else IDEA may complain (see https://youtrack.jetbrains.com/issue/IDEA-317606)
+rootProject.name = "MultiLoader-Monorepo-Project"
+
+val mods = arrayOf("examplemod")
+mods.forEach { mod ->
+    include("$mod:common", "$mod:fabric", "$mod:forge")
+}
